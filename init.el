ff69626722eaf97c5d58dc2c4bf56616db82f02b
Proud Indian <rrangarajan.85@gmail.com>
2017-04-06 15:27:20 +0530

    Update init.el

--- init.el
+++ init.el
@@ -4,7 +4,16 @@
 (require 'bookmark)
 (bookmark-bmenu-list)
 (switch-to-buffer "*Bookmark List*")
-;; feature 0001 - start- Starting Emacs with Bookmark                                              
+;; feature 0001 - end - Starting Emacs with Bookmark  
+
+;; feature 0002 - start - highlighting matching braces
+(setq show-paren-style 'parenthesis) ; highlight brackets
+(setq show-paren-style 'expression) ; highlight entire expression
+(setq show-paren-style 'mixed) ; highlight brackets if visible, else entire expression
+;; feature 0002 - end - highlighting matching braces
+
+;; feature 0003 - start - 
+;; feature 0003 - end - 
 
 ;;Customization                                                                                    
 ;;cust - 0001                                                                                      
@@ -18,3 +27,8 @@
 
 ;;cust - 0004                                                                                      
 (global-set-key "C-n n" 'xah-toggle-read-novel-mode)
+
+;;cust -0005
+;; turn on highlight matching brackets when cursor is on one
+(show-paren-mode 1)
+
